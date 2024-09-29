eddef9be015e4a99d78300491115669c19215086
tamerkat <mahad10taha@gmail.com>
2024-09-29 14:47:32 +0300

    add

---
+++ 0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Island Perimeter."""
+
+
+def island_perimeter(grid):
+    "Return island perimeter defined by grid."
+
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+    return perimeter
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
